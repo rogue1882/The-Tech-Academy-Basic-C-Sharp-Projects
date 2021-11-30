cbc45b09ad0fbba9e999a7bf510cbb20c030c375
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-11-29 20:14:28 -0500

    The rest of the assignment

---
+++ Basic_C#_Programs/Acme_Accounting_Systems/Acme_Accounting_Systems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Acme_Accounting_Systems
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
