ec23e2dac6907b93e47e42c874acabf896e069c1
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-05 11:43:25 -0500

    Console Application Assignment

---
+++ Basic_C#_Programs/ConsoleApplicationAssignment/ConsoleApplicationAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asking for a number and multiply by 50
+            Console.WriteLine("Please enter a number");
+            string number = Console.ReadLine();
+            int Mnumber = Convert.ToInt32(number); 
+            int multiply = Mnumber * 50;
+            Console.WriteLine(multiply);
+            Console.ReadLine();
+
+            //Asks for a number and adds 25 to it
+            Console.WriteLine("Please enter a number");
+            string number2 = Console.ReadLine();
+            int addNumber = Convert.ToInt32(number2);
+            int add = addNumber + 25;
+            Console.WriteLine(add);
+            Console.ReadLine();
+
+            //Asks for a number and divides it by 12.5
+            Console.WriteLine("Please enter a number");
+            string number3 = Console.ReadLine();
+            double divideNumber = Convert.ToDouble(number3); 
+            double divide = divideNumber / 12.5;
+            Console.WriteLine(divide);
+            Console.ReadLine();
+
+            //Asks for a number and number and validates if it is greater than 50
+            Console.WriteLine("Please enter a number");
+            string number4 = Console.ReadLine();
+            int validateNumber = Convert.ToInt32(number4);
+            bool validate = validateNumber > 50;
+            Console.WriteLine(validate);
+            Console.ReadLine();
+
+
+            //Asks for a number and number and validates if it is greater than 50
+            Console.WriteLine("Please enter a number");
+            string number5 = Console.ReadLine();
+            int remainderNumber = Convert.ToInt32(number5);
+            int remainder = remainderNumber % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Math Challenge/Math Challenge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 5;
+            int num2 = 12;
+            int add = num1 + num2;
+            Console.WriteLine(add);
+            Console.ReadLine();
+
+
+            int num3 = 5;
+            int num4= 12;
+            int subtract = num3 - num4;
+            Console.WriteLine(subtract);
+            Console.ReadLine();
+
+            int num5 = 5;
+            int num6 = 12;
+            int multiply = num5 * num6;
+            Console.WriteLine(multiply);
+            Console.ReadLine();
+
+            int num7 = 48;
+            int num8 = 12;
+            int divide = num7 / num8;
+            Console.WriteLine(divide);
+            Console.ReadLine();
+
+            string name = "George";
+            int number = 52;
+            Console.WriteLine(name + number);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
