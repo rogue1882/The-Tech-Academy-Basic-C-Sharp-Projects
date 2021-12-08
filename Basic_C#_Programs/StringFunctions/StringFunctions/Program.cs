5fe02992b65bc76313d0a019ab842b64e08b413a
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-07 21:59:27 -0500

    added to DoWhile

---
+++ Basic_C#_Programs/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The man said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = "C:\\Users\\Jesse";
+            //string fileName1= @"C:\Users\Jesse";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+            //name = name.ToUpper();
+            //name = name.ToLower();
+
+            //string name3 = "Jesse";
+            //name = "Joe";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
--- Basic_C#_Programs/doWhileStatement/doWhileStatement/Program.cs
+++ Basic_C#_Programs/doWhileStatement/doWhileStatement/Program.cs
@@ -49,6 +49,38 @@ namespace doWhileStatement
             while (!employID);
 
             Console.ReadLine();
+
+            Console.WriteLine("Enter your pin.");
+            int pin = Convert.ToInt32(Console.ReadLine());
+            bool myPin = pin == 1050;
+            
+            while (pin != 1050)
+            switch (pin)
+            {
+                case 1001:
+                    Console.WriteLine("Wrong, Please enter your pin");
+                    Console.WriteLine("Enter your pin.");
+                    pin = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 2000:
+                    Console.WriteLine("You are not authorised at this door.  Please go to the front door");
+                    Console.WriteLine("Enter your pin.");
+                    pin = Convert.ToInt32(Console.ReadLine());
+                    break;
+                 case 1050:
+                     Console.WriteLine("Access granted to IT Offices");
+                     myPin = true;
+                     break;
+                 default:
+                    Console.WriteLine("Please report to HR through the main door");
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                        break;
+                            
+                }
+            Console.ReadLine();
+           
+
         }
     }
 }
