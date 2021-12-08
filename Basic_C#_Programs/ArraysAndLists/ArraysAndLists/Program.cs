7ef154dc03a50ef24f0da8e6edf26aa11e18ea0f
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-07 22:16:59 -0500

    arraysand lists

--- Basic_C#_Programs/ArraysAndLists/ArraysAndLists/Program.cs
+++ Basic_C#_Programs/ArraysAndLists/ArraysAndLists/Program.cs
@@ -6,18 +6,42 @@ using System.Collections.Generic;
     {
       static void Main()
         {
-        int[] numArray = new int[5];
-        numArray[0] = 5;
-        numArray[1] = 2;
-        numArray[2] = 10;
-        numArray[3] = 200;
-        numArray[4] = 5000;
+        //List<int> intList = new List<int>();
+        //intList.Add(4);
+        //intList.Add(10);
+        //intList.Remove(10);
+
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("Jesse");
+        intList.Remove("Jesse");
 
 
-        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
 
-        Console.WriteLine(numArray1[3]);
+        Console.WriteLine(intList[0]);
         Console.ReadLine();
+
+        //byte[] byteArray = 
+
+
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //numArray2[5] = 650;
+
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
 
 
         }
