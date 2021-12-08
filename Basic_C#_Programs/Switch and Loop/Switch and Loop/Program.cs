9f38ae47dd3a3afc1fbe99193c3cf6f9a3d9938f
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-07 20:56:52 -0500

    DoWhile Challenge

---
+++ Basic_C#_Programs/Switch and Loop/Switch and Loop/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_and_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int day = 3;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //}
+            //Console.ReadLine();
+
+
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/doWhileStatement/doWhileStatement/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your employee number?");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            bool employID = ID == 8664;
+
+            do
+            {
+
+                switch (ID)
+                {
+                    case 1001:
+                        Console.WriteLine("Employee not authorized. Try again.");
+                        Console.WriteLine("What is your employee number?");
+                        ID = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2005:
+                        Console.WriteLine("Employee not authorized. Try again.");
+                        Console.WriteLine("What is your employee number?");
+                        ID = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6776:
+                        Console.WriteLine("Employee not authorized. Try again.");
+                        Console.WriteLine("What is your employee number?");
+                        ID = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8664:
+                        Console.WriteLine(" You may enter");
+                        employID = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("What is your employee number?");
+                        ID = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+            while (!employID);
+
+            Console.ReadLine();
+        }
+    }
+}
