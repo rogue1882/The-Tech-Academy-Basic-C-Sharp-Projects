b67cba8013e4d5ff4ad1a6da0b8c88cfea93b64f
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-13 21:18:53 -0500

    console app part one issues

---
+++ Basic_C#_Programs/Array App/Array App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Jeff", "John", "Jimmy", "Jill", "Jennifer" };
+            Console.WriteLine("What is your last name? ");
+            string Lname = Console.ReadLine();
+            Console.ReadLine();
+
+            for (int i = 0; i < names.Length; i++)
+
+            {
+              Console.WriteLine(names[i]);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
