82f76c3b6243e59dd6c8839b886887af88e85eeb
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-20 23:35:14 -0500

    Finished App Assognment

--- Basic_C#_Programs/Array App/Array App/Program.cs
+++ Basic_C#_Programs/Array App/Array App/Program.cs
@@ -10,97 +10,106 @@ namespace Array_App
     {
         static void Main(string[] args)
         {
-            //string[] names = { "Jeff", "John", "Jimmy", "Jill", "Jennifer" };
-            //Console.WriteLine("What is your last name? ");
-            //string Lname = Console.ReadLine();
-            //Console.ReadLine();
+            string[] names = { "Jeff", "John", "Jimmy", "Jill", "Jennifer" };
+            Console.WriteLine("What is your last name? ");
+            string Lname = Console.ReadLine();
+            Console.ReadLine();
 
-            //for (int i = 0; i < names.Length; i++)
+            for (int i = 0; i < names.Length; i++)
 
-            //{
-            //    names[i] += " " + Lname;
-            //}
+            {
+                names[i] += " " + Lname;
+            }
 
-            //for (int i = 0; i < names.Length; i++)
-            //{
-            //    Console.WriteLine(names[i]);
-            //}
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
 
-            //    Console.ReadLine();
+            Console.ReadLine();
 
-            //int[] num1 = { 10, 25, 53, 25, 17, 83, 100 };
+            int[] num1 = { 10, 25, 53, 25, 17, 83, 100 };
 
-            //for (int i = 0; i < num1.Length; i++)
-            //{
-            //    if (num1[i] < 25)
-            //    {
-            //        Console.WriteLine(num1[i]);
-            //    }
+            for (int i = 0; i < num1.Length; i++)
+            {
+                if (num1[i] < 25)
+                {
+                    Console.WriteLine(num1[i]);
+                }
 
-            //    Console.ReadLine();
+                Console.ReadLine();
 
 
-            //}
+            }
 
-            //int[] num2 = { 10, 25, 53, 25, 17, 83, 100 };
+            int[] num2 = { 10, 25, 53, 25, 17, 83, 100 };
 
-            //for (int i = 0; i < num2.Length; i++)
-            //{
-            //    if (num2[i] <= 25)
-            //    {
-            //        Console.WriteLine(num2[i]);
-            //    }
+            for (int i = 0; i < num2.Length; i++)
+            {
+                if (num2[i] <= 25)
+                {
+                    Console.WriteLine(num2[i]);
+                }
 
-            //    Console.ReadLine();
-            //}
+                Console.ReadLine();
+            }
 
-            //string[] cars = { "ford", "chevy", "mustang", "chevelle", "corvert", "toyota" };
-            //Console.WriteLine("What is your car?");
-            //string myCar = (Console.ReadLine());
+            string[] cars1 = { "ford", "chevy", "mustang", "chevelle", "corvert", "toyota" };
+            Console.WriteLine("What is your car?");
+            string myCar1 = (Console.ReadLine());
 
-            //for (int J = 0; J < cars.Length; J++ )
-            //{
-            //    if (cars[J] == myCar) 
-            //        {
-            //            Console.WriteLine("Your car is in this list");
-            //            Console.ReadLine();
-            //            break;
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Your car is not on this list");
-            //            Console.ReadLine();
-            //        break;
-            //        }
-            //}
+            for (int J = 0; J < cars1.Length; J++)
+            {
+                if (cars1[J] == myCar1)
+                {
+                    Console.WriteLine("Your car is in this list");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your car is not on this list");
+                    Console.ReadLine();
+                    break;
+                }
+            }
 
-            List<string> cars =  new List<string> () { "ford", "chevy", "mustang", "chevelle", "corvert", "toyota", "ford"};
+            List<string> cars = new List<string>() { "ford", "chevy", "mustang", "chevelle", "corvert", "toyota", "ford" };
             Console.WriteLine("What is the make of your car?");
             string myCar = (Console.ReadLine());
-            Console.ReadLine();
-
-
-            
-                
-            
-                foreach (string car in cars)
-
-                {
+            if (cars.Contains(myCar))
+            {
                 for (int i = 0; i < cars.Count; i++)
                 {
-                    if (myCar == car)
+                    if (myCar == cars[i])
+
                     {
-                        Console.WriteLine("your car is listed at index " + cars.Count);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please try again");
+                        Console.WriteLine("your car is listed at index " + i);
+
                     }
                 }
-                }
-          
+            }
+            else
+            {
+                Console.WriteLine("Your car is not on the list");
+
+            }
+
             Console.ReadLine();
 
+            List<string> dupeCars = new List<string>();
+            foreach (string car in cars) { 
+
+                if (myCar == car)
+                {
+                    if (dupeCars.Contains(car))
+                    {
+                        Console.WriteLine("This car is a duplicate " + car);
+                    }
+                    dupeCars.Add(car);
+                }
+        } Console.ReadLine();
+    
 
 
 
