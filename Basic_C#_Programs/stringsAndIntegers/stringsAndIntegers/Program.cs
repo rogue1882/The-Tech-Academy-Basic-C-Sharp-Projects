b20197b1f571e18e75b4bac06ed9704979385d58
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-22 19:09:14 -0500

    StringAndIntegers Assignment

---
+++ Basic_C#_Programs/stringsAndIntegers/stringsAndIntegers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringsAndIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try statement to test loop
+            try
+            {
+                // Block creates lista and ask user for a number to divide by
+                // converts the  string to int and the iterates through the list
+                //displays all indexes divided on screen
+                List<int> num1 = new List<int> { 1, 2, 15, 200, 583, 1882 };
+                Console.WriteLine("Enter number to divide by.");
+                int divider = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < num1.Count; i++)
+                {
+                    Console.WriteLine(num1[i] / divider);
+                }
+
+            }
+                //Catch block for type errors
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a  whole number");
+                
+            }
+                //Catch block for zeros
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+                //Catch block for other exceptions
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+                //Close out with display
+            finally
+            {
+                Console.WriteLine("You have emerged from a try/catch block and continued on with program execution");
+                Console.ReadLine();
+            }
+        }
+            
+    }
+}
