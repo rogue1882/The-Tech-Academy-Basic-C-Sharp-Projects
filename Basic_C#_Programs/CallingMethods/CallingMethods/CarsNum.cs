0de0ae772074db7b7bc1fc83098c2e7a8c9910ae
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-26 07:43:43 -0600

    Calling methods

---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/CarsNum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class CarsNum
+    {
+        BlueCar = 10;
+        WhiteCar = 20;
+        GrayCar = 2;
+    }
+    public int BlueCar { get; set; }
+    public int WhiteCar { get; set; }
+}
---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number for the math program test.");
+            {
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+            }
+        }
+
+        public static Math()
+            {
+            int Num2 = 2246;
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            int Num3 = Num2 / Num1;
+            Console.WriteLine(Num3);
+            _ = Console.ReadLine();
+            }
+        
+        
+    }
+    
+}
+    
+
