4e3d30a6d36a4ab1fc60d1d96b5f356c59346037
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-12-30 20:24:12 -0500

    Main Method question

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //request user input
+            Console.WriteLine("Please enter a number. ");
+            {
+                //Convert to integer
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+                //Calling the add method
+                Console.WriteLine(Submission.AddNumber(userNum));
+
+                //Calling the decimal subtraction method
+                Console.WriteLine(Submission.addNumber(userNum));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Submission.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Submission
+    {
+        //Add method
+        public static int AddNumber(int userNum)
+        {
+            int newNum = userNum + 10;
+            return newNum;
+
+        }
+
+        //Subtraction method
+        public static int addNumber(decimal userNum)
+        {
+            decimal newNum2 = userNum - 10;
+            return (int)newNum2;
+        }
+
+        //Multiply method
+        public static string AddNumber(string userNum)
+        {
+            int userNum4 = Convert.ToInt32(userNum);
+            int newNum3 = userNum4 + 2;
+            return newNum3;
+        }
+    }
+}
