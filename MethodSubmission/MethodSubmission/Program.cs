37e6d7215c38699441f227a9a109cb2471b34e18
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-01-02 15:00:17 -0500

    Added Class method

--- MethodSubmission/MethodSubmission/Program.cs
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -8,8 +8,8 @@ namespace MethodSubmission
 {
     class Program
     {
-        public static int Num2 { get; private set; }
-        public static int userNum { get; private set; }
+        //public static int Num2 { get; private set; }
+        //public static int userNum { get; private set; }
 
         public static void Main(string[] args)
         {
@@ -19,20 +19,26 @@ namespace MethodSubmission
                 //Convert to integer
                 int userNum = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine("Add a second number.");
 
+                //Convert to integer
+                int Num2 = Convert.ToInt32(Console.ReadLine());
+
+                //calling void method
+                Console.WriteLine(Test.TestNumber(userNum, Num2));
 
                 //request user to give a second int input or let computer pick random int
-                Console.WriteLine("Would you like to enter another number? Yes or No");
-                bool Yes = Convert.ToBoolean(Console.ReadLine());
-                 if (Yes != true)
-                    {
-                    Console.WriteLine("The computer picks number 12");
-                    int Num2 =  12;
-                    }
-                
+                //Console.WriteLine("Would you like to enter another number? Yes or No");
+                //bool Yes = Convert.ToBoolean(Console.ReadLine());
+                // if (Yes != true)
+                //    {
+                //    Console.WriteLine("The computer picks number 12");
+                //    int Num2 =  12;
+                //    }
+
 
                 // Calling Method AddNumber
-                Console.WriteLine(takeTwo.AddNumber(userNum, Num2));
+                //Console.WriteLine(takeTwo.AddNumber(userNum, Num2));
 
             }
             Console.ReadLine();
---
+++ MethodSubmission/MethodSubmission/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Test
+    {
+        public static int TestNumber(int userNum, int Num2)
+        {
+            int newNum = userNum + 12;
+            return Num2;
+            
+        }
+    }
+}
