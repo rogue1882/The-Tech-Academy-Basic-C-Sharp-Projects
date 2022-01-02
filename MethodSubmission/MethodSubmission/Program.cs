53f324a9b4e85a6383b451f2e067205192ea82be
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-01-02 12:12:48 -0500

    Method Submission question

---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        public static int Num2 { get; private set; }
+        public static int userNum { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            //request user input
+            Console.WriteLine("Please enter a number. ");
+            {
+                //Convert to integer
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+                //request user to give a second int input or let computer pick random int
+                Console.WriteLine("Would you like to enter another number? Yes or No");
+                bool Yes = Convert.ToBoolean(Console.ReadLine());
+                 if (Yes != true)
+                    {
+                    Console.WriteLine("The computer picks number 12");
+                    int Num2 =  12;
+                    }
+                
+
+                // Calling Method AddNumber
+                Console.WriteLine(takeTwo.AddNumber(userNum, Num2));
+
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ MethodSubmission/MethodSubmission/takeTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class takeTwo
+    {
+        //Add method taking in 2 provided int's
+        public static int AddNumber(int userNum , int Num2)
+        {
+            int newNum = userNum + Num2;
+            return newNum;
+
+        }
+    }
+}
