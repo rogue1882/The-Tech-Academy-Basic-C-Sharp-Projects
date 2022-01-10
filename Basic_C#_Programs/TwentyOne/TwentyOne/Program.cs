2d14d2016f978dc7f4e8380150491582279e013d
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-01-09 20:25:30 -0500

    21

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -15,6 +15,14 @@ namespace TwentyOne
             //game.ListPlayers();
             //Console.ReadLine();
 
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Jesse";
+            game += player;
+            game -= player;
+
+
 
             Deck deck = new Deck();
             deck.Shuffle(3);
