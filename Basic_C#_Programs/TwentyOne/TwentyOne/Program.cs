938fc8af8ac3d69374262e9f350f318285df3c48
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-02-18 18:30:56 -0500

    Struct Assignment

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -25,15 +25,24 @@ namespace TwentyOne
             //Card card = new Card();
             //card.Suit = Suit.Clubs;
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
 
-            Console.WriteLine(deck.Cards.Count);
+            Card card1 = new Card();
+            Card card2 = card1;
+            card1.Face = Face.Eight;
+            card2.Face = Face.King;
+
+            Console.WriteLine(card1.Face);
+
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+
+            //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
 
