cbe12bda58904c38801e8212c65f3db32917f923
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-02-27 16:41:40 -0500

    21 update

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; 
 
 namespace TwentyOne
 {
@@ -10,29 +11,33 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            
+
             Console.WriteLine("Welcome to the Grand Hotel and Casino.  Let's start by telling me your name.");
             string playerName = Console.ReadLine();
+
             Console.WriteLine("And how much did you bring today?");
             int bank = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Hello, {0}.  Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
-            if (answer == "yes" || answer == "yeah" || answer = "y" || answer == "ya")
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
                 Game game = new TwentyOneGame();
                 game += player;
                 player.isActivelyPlaying = true;
+                
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
                     game.Play();
                 }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
 
             }
-
-
-
-
-
+            Console.WriteLine("Feel free to look around the casino.  Bye for now.");
+            Console.ReadLine();
 
             //TwentyOneGame game = new TwentyOneGame();
             //game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
