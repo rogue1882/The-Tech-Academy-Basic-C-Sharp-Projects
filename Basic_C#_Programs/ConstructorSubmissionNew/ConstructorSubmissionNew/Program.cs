4071b88b4d690d2db7cec2cda1413432fe399221
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-03-06 14:20:15 -0500

    Stack overflow issue

---
+++ Basic_C#_Programs/ConstructorSubmissionNew/ConstructorSubmissionNew/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionNew
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding Constant to program. 
+            const string hotelName = "Grand Budepest Inn";
+
+            // Creating opening statement and question
+            Console.WriteLine("Welcome to {0}.  Would you like to make a reservation?", hotelName);
+            Console.ReadLine();
+             
+            //getting information for reservation
+            Console.WriteLine("What name would you like the reservation under?");
+            string Name = (Console.ReadLine());
+            Reservation reservation = new Reservation(Name);
+
+            Console.WriteLine("Your reservation at the {0} is under the name of {1}.  Thank you", hotelName, reservation);
+            Console.ReadLine();
+
+
+           
+
+
+        }
+        
+    }
+}
---
+++ Basic_C#_Programs/ConstructorSubmissionNew/ConstructorSubmissionNew/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionNew
+{
+    public class Reservation
+    {
+        Reservation Reservations = new Reservation();
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(object name)
+        {
+            Name = name;
+        }
+
+        public object Name { get; }
+    }
+}
