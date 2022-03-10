029388e61f21cad2a76286732a56d422b54aed70
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-03-09 20:28:51 -0500

    started

---
+++ Basic_C#_Programs/TryCatchExceptionAssignment/TryCatchExceptionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your Age?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Age);
+            Console.ReadLine();
+
+            var myDate = DateTime.Now;
+            var newDate = myDate.AddYears(-Age);
+            Console.WriteLine(newDate);
+            Console.ReadLine();
+        }
+    }
+}
