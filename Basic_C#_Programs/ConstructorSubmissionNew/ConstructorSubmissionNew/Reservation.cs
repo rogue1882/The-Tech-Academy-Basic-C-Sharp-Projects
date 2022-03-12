21ae2639aae4baff5f22ddda1f46d67d8f44b571
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-03-12 08:39:48 -0600

    Capitalization corrections

--- Basic_C#_Programs/ConstructorSubmissionNew/ConstructorSubmissionNew/Reservation.cs
+++ Basic_C#_Programs/ConstructorSubmissionNew/ConstructorSubmissionNew/Reservation.cs
@@ -24,7 +24,7 @@ namespace ConstructorSubmissionNew
         public Reservation( string name = "", string ID = "")
         {
             this.ID = ID;
-            this.Name = Name;
+            this.Name = name;
         }
 
         
