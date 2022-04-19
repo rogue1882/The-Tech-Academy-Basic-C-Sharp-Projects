6f397f876be4001760cb64afa85641d5ae993a3f
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2022-04-19 19:01:14 -0400

    adds

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -23,6 +23,20 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the {0}.  Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + "|");
+                    Console.Write(exception.ExceptionType + "|");
+                    Console.Write(exception.ExceptionMessage + "|");
+                    Console.Write(exception.TimeStamp + "|");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
 
             bool validAnswer = false;
             int bank = 0;
@@ -54,15 +68,17 @@ namespace TwentyOne
                     {
                         game.Play();
                     }
-                    catch (FraudException)
+                    catch (FraudException ex)
                     {
-                        Console.WriteLine("Security! Kick this person out.");
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         Console.WriteLine("An error occured.  Please contact your System Administrator.");
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
@@ -86,7 +102,7 @@ namespace TwentyOne
             string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) Values
                                  (@ExceptionType, @ExceptionMessage, @TimeStamp)";
 
-            using (SqlConnection connection = new SqlConnection(connectionString)) 
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand(queryString, connection);
                 command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
@@ -99,10 +115,44 @@ namespace TwentyOne
 
                 connection.Open();
                 command.ExecuteNonQuery();
+                connection.Close();
 
             }
 
         }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;           
+                                    Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                    TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                    MultiSubnetFailover=False";
 
+            string queryString = @"Select Id, ExceptionType, ExecptionMessage, TimeStamp from Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+
+            }
+            return Exceptions;
+        }
     }
 }
+
---
+++ Basic_C#_Programs/WebApplication2/WebApplication2B/WebApplication2B/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication2B.Global" Language="C#" %>
